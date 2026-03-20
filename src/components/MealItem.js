dbab758cd808392a1cc463e4319dbaf2ee270961
Lauri <lauri.vosmi@voco.ee>
2026-03-20 11:42:26 +0200

    ylesanne 5

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,11 +1,18 @@
+const currencyFormatter = new Intl.NumberFormat("et-EE", {
+    style: "currency",
+    currency: "EUR",
+});
+
 const MealItem = (props) => {
+    const formattedPrice = currencyFormatter.format(Number(props.meal.price));
+
     return (
         <li className="meal-item">
             <article>
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3>{props.meal.name}</h3>
-                    <p className="meal-item-price">{props.meal.price}</p>
+                    <p className="meal-item-price">{formattedPrice}</p>
                     <p className="meal-item-description">{props.meal.description}</p>
                 </div>
                 <p className="meal-item-actions">
